e95748ce2499279e44598180bd9bca1c637ba601
Brandon <brandonshen123@gmail.com>
2024-07-15 11:40:39 -0700

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,7 @@
 import { Container, Row, Col } from "react-bootstrap";
-import logo from "../assets/img/logo.svg";
-import navIcon1 from "../assets/img/nav-icon1.svg";
-import navIcon2 from "../assets/img/nav-icon2.svg";
-import navIcon3 from "../assets/img/nav-icon3.svg";
+import navIcon1 from '../assets/img/nav-icon1.svg';
+import navIcon2 from '../assets/img/github-mark-white.svg';
+import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const Footer = () => {
   return (
@@ -10,15 +9,15 @@ export const Footer = () => {
       <Container>
         <Row className="align-items-center">
           <Col size={12} sm={6}>
-            <img src={logo} alt="Logo" />
+          <h2 className="logoText"><b>Brandon Shen</b></h2>
           </Col>
           <Col size={12} sm={6} className="text-center text-sm-end">
             <div className="social-icon">
-              <a href="#"><img src={navIcon1} alt="Icon" /></a>
-              <a href="#"><img src={navIcon2} alt="Icon" /></a>
-              <a href="#"><img src={navIcon3} alt="Icon" /></a>
+                <a href="https://www.linkedin.com/in/brandon-shen-b7261b2b7/" target="_blank"><img src={navIcon1} alt="Linkedin" /></a>
+                <a href="https://github.com/BrandonS09" target="_blank"><img src={navIcon2} alt="Github" /></a>
+                <a href="https://www.instagram.com/_brandon.s__/" target="_blank"><img src={navIcon3} alt="Instagram" /></a>
             </div>
-            <p>Copyright 2022. All Rights Reserved</p>
+            <p>Copyright 2024. All Rights Reserved</p>
           </Col>
         </Row>
       </Container>
